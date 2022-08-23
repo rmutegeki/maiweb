2a21c0dbff447167a5d430c9b01eec8338f8c36e
Ronnie <murogive@gmail.com>
2022-08-23 04:11:53 +0100

    Added default files.

---
+++ maicat_login/main.js
@@ -0,0 +1,56 @@
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+       	return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'text' || $(input).attr('name') == 'id') {
+            if($(input).val().trim() == '' || $(input).val().trim() != 'maibot'){
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == '' || $(input).val().trim() != 'maibot'){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
